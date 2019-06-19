de507c1e5e0d6b2a8e3fac792ddaebb26889d856
Abdullah Haneef <abddagia@gmail.com>
2019-06-20 04:04:51 +0500

    uodadasd

--- server.js
+++ server.js
@@ -168,6 +168,41 @@ app.post("/addClass", function(req, res) {
     })        
 });
 
+app.post("/searchStd", function(req, res) {
+    setupResponse(res);
+    console.log(Object.keys(req.body.data).length)
+
+    var students;
+    var query_get_student = ""
+    if(req.body.data["nothing"]){
+        var query_get_student = "SELECT *\
+                            FROM student;"
+    }
+    else{
+        var query_get_student = "SELECT * FROM student WHERE "
+        for (i=0;i<Object.keys(req.body.data).length;i++){
+            var accessKey = Object.keys(req.body.data)[i];
+            query_get_student = query_get_student + accessKey +" = '" + req.body.data[accessKey] + "'  AND "
+        }
+        query_get_student  = query_get_student.substring(0, query_get_student.length - 6);
+        query_get_student = query_get_student + ";";
+    }
+    console.log(query_get_student);
+    client.query(query_get_student, function(err, result) {
+        if(err) {
+            console.log(err)
+        }
+        else {
+            for (i=0; i<result.rows.length; i++){
+                students = result.rows;
+            }
+            client.end();
+            res.end(JSON.stringify({"status":"success", "students":students}));
+        }
+    });        
+});
+
+
 // route for user logout
 app.get('/logout', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
@@ -217,6 +252,42 @@ app.get("/get_class", function(req, res) {
     });
 });
 
+app.post("/update_student_fee", jsonParser, function(req, res) {
+    setupResponse(res);
+    console.log(req.body);
+    console.log(Object.keys(req.body).length);
+    for (i=0;i<Object.keys(req.body).length;i++){
+        console.log(Object.keys(req.body)[i])
+        var accessKey = Object.keys(req.body)[i];
+        if(req.body[accessKey] == ""){
+            req.body[accessKey] = 0;
+        }
+        //var accessKey = Object.keys(req.body.data)[i];
+        //query_get_student = query_get_student + accessKey +" = '" + req.body.data[accessKey] + "'  AND "
+    }
+    var query_update_student = "UPDATE student SET \
+                                admission_fees="+req.body['admission_fees']+", \
+                                monthly_fees="+req.body['monthly_fees']+", \
+                                arrears="+req.body['arrears']+", \
+                                security_fees="+req.body['security_fees']+", \
+                                annual_fees="+req.body['annual_fees']+", \
+                                misc_fees="+req.body['misc_fees']+", \
+                                current_penalty="+req.body['current_penalty']+" \
+                                WHERE id="+req.body['stdId']+";"
+    console.log(query_update_student);
+    client.query(query_update_student, function(err, result) {
+        if(err) {
+            console.log(err)
+        }
+        else {
+            client.end();
+            res.end(JSON.stringify({"status":"success"}));
+        }
+    });
+    //query_get_student  = query_get_student.substring(0, query_get_student.length - 6);
+    //query_get_student = query_get_student + ";";    
+    //res.end(JSON.stringify({"status":"success"}));
+});
 
 app.post("/print_challan", jsonParser, function(req, res) {
     setupResponse(res);
